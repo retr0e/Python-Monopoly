f04855fba1a565abfe518f8120fd5e4afb998687
Dawid Śleziak <dawid.sleziak@gmail.com>
2023-11-06 23:25:19 +0100

    Creating basic class

---
+++ main.py
@@ -0,0 +1,44 @@
+# class Bank:
+class Player:
+    nick = 'default_name'
+    money = 0
+    owned_properties = []
+
+
+class Field:
+    id = 0
+    name = 'field_name'
+
+
+class Tax:
+    money_hit = 0
+
+
+class Start:
+    bonus = 200
+
+
+class Parking:
+    payback = 0
+
+
+class ActiveFields:
+    purchase = 0
+    rent = []
+    Owner = None
+    Mortgage = 0
+    Mortgage_buyout = 0
+
+
+class Property:
+    houses = 0
+    available_hotel = False
+    hotel_price = 0
+    house_price = 0
+
+
+class Utility:
+    bonus = 0
+
+
+
