14293c6999b0e32b0c3a0b7e1bef1249f3eda7ff
Dawid Śleziak <dawid.sleziak@gmail.com>
2023-11-07 00:04:27 +0100

    Constructor of ActiveField class

--- active_fields.py
+++ active_fields.py
@@ -3,7 +3,16 @@ from field import Field
 
 class ActiveFields(Field):
     purchase = 0
-    rent = []
-    Owner = None
-    Mortgage = 0
-    Mortgage_buyout = 0
+    rents = []
+    owner = None
+    mortgage = 0
+    mortgage_buyout = 0
+
+    def __init__(self, identification, name, property_cost, rent, mortgage, mortgage_to_pay):
+        self.id = identification
+        self.name = name
+        self.purchase = property_cost
+        self.rents = rent
+        self.mortgage = mortgage
+        self.mortgage_buyout = mortgage_to_pay
+        
