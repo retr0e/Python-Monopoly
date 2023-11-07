07e3f1150a87455dc11da364838c22b27d07ba1d
Dawid Śleziak <dawid.sleziak@gmail.com>
2023-11-07 13:44:15 +0100

    Utility constructor

--- utility.py
+++ utility.py
@@ -3,3 +3,12 @@ from active_fields import ActiveFields
 
 class Utility(ActiveFields):
     bonus = 0
+
+    def __init__(self, identification, name, buyout):
+        super().__init__(identification, name, buyout, 0)
+        self.bonus = 4
+
+    @staticmethod
+    def create_utilities():
+        Utility(12, 'ELEKTROWNIA', 150)
+        Utility(28, 'WODOCIAGI', 150)
