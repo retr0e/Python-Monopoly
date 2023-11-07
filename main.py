7bd33cc432d35b8d6b1e7c971ac6dfa325df9f6d
Dawid Śleziak <dawid.sleziak@gmail.com>
2023-11-07 15:17:48 +0100

    Creating players and menu show

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# ------------ IMPORTS ------------
+
 from active_fields import ActiveFields
 from field import Field
 from parking import Parking
@@ -7,3 +9,25 @@ from start import Start
 from tax import Tax
 from utility import Utility
 from bank import Bank
+
+# ------------ FUNCTIONS ------------
+
+
+def show_menu():
+
+    print('WITAJ W MONOPOLY!')
+    print('1 - Rozpocznij rozgrywke')
+    print('2 - Wczytaj poprzednią rozgrywke')
+    print('3 - Zasady obecnej wersji gry')
+    print('4 - Nie mam czasu na to! Wyjdz.')
+
+
+# ------------ MAIN PROGRAM ------------
+
+show_menu()
+play_monopoly = True
+
+while play_monopoly:
+    print(Player.create_players())
+
+    play_monopoly = False
--- player.py
+++ player.py
@@ -6,4 +6,17 @@ class Player:
 
     def __init__(self, nick_name):
         self.nick = nick_name
-        
+
+    @staticmethod
+    def create_players():
+        players_amount = int(input('Ile osob będzie grac? (Max: 6)\n'))
+        while players_amount < 2 or players_amount > 6:
+            players_amount = int(input('Proszę wprowadz ilosc, ktora jest dopuszczalna\n(Min: 2)\n(Max: 6)\n'))
+
+        players_dict = {}
+        for i in range(0, players_amount):
+            player_nickname = input('Jak bedzie nazywać się gracz P' + str(i + 1) + ': ')
+            players_dict.update({'Player' + str(i): Player(player_nickname)})
+
+        return players_dict
+
